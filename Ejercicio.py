af36ab00d828fc5802cec06bafdfcab5c66e2000
Fer-06 <jeguerrag@unitru.edu.pe>
2025-01-20 01:36:13 +0000

    Practica1

---
+++ Ejercicio.py
@@ -0,0 +1,5 @@
+print("Hello word")
+print("Hola mundo")
+cadena1="Hola"
+cadena2="Mundo"
+print(cadena1,cadena2)
---
+++ Ejercicio5.py
@@ -0,0 +1,3 @@
+numero_entero=int(input("Ingrese numero entero: "))
+suma=numero_entero*(numero_entero+1)//2
+print("El resultado de la suma desde 1 hasta"+" "+ str(numero_entero) +" " +"es"+ " :"+ str(suma))
